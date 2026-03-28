ceabcf3cd940ba4fa41a90317cc8d4feb23a6cbc
Suren Adzhamoglyan <adgsuren@gmail.com>
2026-03-28 13:40:38 -0700

    remove db/ from tracking and add to gitignore

--- db/schema/posts.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE posts (
-    id BIGSERIAL PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE,
-    user_id bigint not null
-);
-insert into posts (name, user_id) values ("asdasd", 23)
--- db/schema/users.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE,
-    age integer
-);
